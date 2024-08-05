00b420d43b14832ace11b473da76e55bbafd07a2
NjoguRufus <rsgxliv@gmail.com>
2024-08-05 19:58:26 +0300

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-300 text-gray-700 p-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 Food Donation App. All rights reserved.</p>
+        <p className="mt-2">Created by Rufus and Team</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
