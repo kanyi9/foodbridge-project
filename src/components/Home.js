891760db1a2b784f042dabe2892f506fd42d58e4
Peter <peterkanyi19193@gmail.com>
2024-08-06 09:36:24 +0300

    adds login and register forms

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './Login';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the home page!</h1>
+     
+      <Login />
+    </div>
+  );
+}
+
+export default Home;
