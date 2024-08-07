25854e219faed1f27e5337060dc506ebf8a76a59
NjoguRufus <rsgxliv@gmail.com>
2024-08-07 12:10:04 +0300

    Campaigns

--- src/App.js
+++ src/App.js
@@ -4,18 +4,28 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import {Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import LoginPage from './components/Login';
+import SignupPage from './components/SignUp';
+import Footer from './components/Footer';
+import TeamSection from './components/TeamSection';
+import Campaigns from './components/Campaigns';
 
 function App() {
   return (
-    
+    <div>
     <div>
       <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/signup" element={<SignupPage/>} />
       </Routes>
+</div>
+<div>
+  <Campaigns />
+  <TeamSection />
+  <Footer />
 
-
+ </div>
+ </div>
   );
 }
 
