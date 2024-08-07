c4b4a4934b8a76fc9fd5f7d14af7c839b1b90486
brianchege2k <chegebrian84@gmail.com>
2024-08-07 11:13:32 +0300

    signin and sign up pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import Footer from './components/Footer';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import {Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import LoginPage from './components/Login';
+import SignupPage from './components/SignUp';
+
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route path="/login" element={<LoginPage/>} />
+        <Route path="/signup" element={<SignupPage/>} />
       </Routes>
 
 
