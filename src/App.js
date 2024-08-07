410a2d3e4cfa6d5051c917ecb0aaad642bd0069d
NjoguRufus <rsgxliv@gmail.com>
2024-08-07 11:06:31 +0300

    Volunters

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import {Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import LoginPage from './components/Login';
-import TeamSection from '.components/TeamSection';
+import TeamSection from './components/TeamSection';
 
 function App() {
   return (
+    
     <div>
       <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route path="/login" element={<LoginPage/>} />
       </Routes>
+     
       <TeamSection />
      <Footer />
     </div>
+  
      
   );
 }
