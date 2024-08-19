bff1ab0f68cbca5ec8b6e995909c0e839fc90c18
CYRUS WACHIRA <mugowachira254@gmail.com>
2024-08-19 08:44:30 +0300

    dealing

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import backgroundImage from '../images/login-background.png';
+
+function InputField({ label, placeholder, type = 'text', value, onChange }) {
+  return (
+    <div className="flex flex-col mt-5 w-full font-medium">
+      <label className="self-start text-sm text-gray-700">
+        {label}
+      </label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="py-2.5 px-3 w-full text-sm rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+        aria-label={label}
+      />
+    </div>
+  );
+}
+
+function Button({ text }) {
+  return (
+    <div className="mt-8">
+      <button className="w-full py-3 bg-yellow-500 text-white font-bold rounded-lg hover:bg-yellow-600 transition-colors">
+        {text}
+      </button>
+    </div>
+  );
+}
+
+function SignupPage() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();  
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('https://foodbridge-backend-bd8l.onrender.com/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, email, password }),
+      });
+      const result = await response.json();
+      if (response.ok) {
+        toast.success('Account created, redirecting...', {
+          position: 'top-center', // Default position as a string
+          autoClose: 3000, // Close after 3 seconds
+        });
+        setTimeout(() => {
+          navigate('/login');
+        }, 3000); // Redirect after 3 seconds
+      } else {
+        setError(result.error || 'Something went wrong');
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="flex min-h-screen">
+      <div className="w-1/2 hidden md:block">
+        <img src={backgroundImage} alt="Background" className="w-full h-full object-cover" />
+      </div>
+
+      <div className="flex items-center justify-center w-full md:w-1/2 bg-white px-10 py-12 md:px-16">
+        <div className="bg-white p-8 rounded-3xl shadow-xl w-full">
+          <h1 className="text-4xl font-bold text-gray-800 text-center mb-8">Join Us Now</h1>
+          <form onSubmit={handleSubmit}>
+            <InputField
+              label="Name"
+              placeholder="Enter your name"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <InputField
+              label="Email address"
+              placeholder="Enter your email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <InputField
+              label="Password"
+              placeholder="Enter your password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error && <p className="text-red-500 mt-4">{error}</p>}
+            <div className="flex items-center mt-5">
+              <input type="checkbox" className="h-4 w-4 text-yellow-600 focus:ring-yellow-500 border-gray-300 rounded" />
+              <label className="ml-2 text-gray-700 text-sm">
+                I agree to the <a href="/terms-and-conditions" className="underline">terms & policy</a>
+              </label>
+            </div>
+            <Button text="Signup" />
+          </form>
+          <div className="relative flex justify-center mt-8">
+            <span className="bg-white px-2 text-gray-500">Or</span>
+          </div>
+
+          <p className="mt-8 text-center text-gray-700">
+            Have an account? <Link to="/login" className="text-yellow-500 font-semibold">Sign In</Link>
+          </p>
+        </div>
+      </div>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default SignupPage;
